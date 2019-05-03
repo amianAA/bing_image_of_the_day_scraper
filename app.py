08ef4e6c4cfbf962ade12eb89b17671d3b54a454
aamian <amianalvaro@gmail.com>
2019-05-03 11:32:07 +0200

    Avoid duplicated files creation

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import requests
 def get_md5(file):
     return hashlib.md5(file).hexdigest()
 
+
 PATH = 'Fotos/Wallpapers/'  # Path inside $HOME folder to store downloaded pictures
 
 AMOUNT = 10  # Amount of pictures
@@ -17,8 +18,22 @@ MARKETS = ['ar-XA', 'bg-BG', 'cs-CZ', 'da-DK', 'de-AT', 'de-CH', 'de-DE', 'el-GR
            'it-IT', 'ja-JP', 'ko-KR', 'lt-LT', 'lv-LV', 'nb-NO', 'nl-BE', 'nl-NL', 'pl-PL', 'pt-BR', 'pt-PT', 'ro-RO',
            'ru-RU', 'sk-SK', 'sl-SL', 'sv-SE', 'th-TH', 'tr-TR', 'uk-UA', 'zh-CN', 'zh-HK', 'zh-TW']
 
-
+processed_hashes = []
 url = 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n={}&mkt={}'
+home = os.path.expanduser('~')
+folder_path = '{}/{}'.format(home, PATH)
+existing_files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+
+for file_name in existing_files:
+    full_file_path = '{}{}'.format(folder_path, file_name)
+    file = open(full_file_path, 'rb')
+    md5 = get_md5(file.read())
+    if md5 in processed_hashes:
+        print('Removed file {}'.format(file_name))
+        os.remove(full_file_path)
+    else:
+        processed_hashes.append(md5)
+    file.close()
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0',
@@ -28,7 +43,6 @@ headers = {
     'Upgrade-Insecure-Requests': '1',
 }
 
-processed_hashes = []
 for mkt in MARKETS:
     response = requests.get(url.format(AMOUNT, mkt), headers=headers)
 
@@ -43,9 +57,7 @@ for mkt in MARKETS:
             md5 = get_md5(image.content)
             if md5 not in processed_hashes:
                 processed_hashes.append(md5)
-
-                home = os.path.expanduser('~')
-                filepath = '{}/{}{}.jpg'.format(home, PATH, file_name)
+                filepath = '{}{}.jpg'.format(folder_path, file_name)
                 os.makedirs(os.path.dirname(filepath), exist_ok=True)
                 with open(filepath, "wb") as f:
                     f.write(image.content)
